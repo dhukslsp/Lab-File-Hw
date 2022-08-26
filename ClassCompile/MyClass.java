4da3dbecee00ab8629c182a72ada0f811833a4d0
Dhruv SIngh <dhruv1647>
2022-08-26 14:46:16 +0530

    Till Runtime Polymorphism

--- ClassCompile/MyClass.java
+++
@@ -1,3 +0,0 @@
-public class MyClass {
-    int x = 10;
-}
--- ClassCompile/SecondClass.java
+++
@@ -1,6 +0,0 @@
-public class SecondClass {
-    public static void main(String args[]){
-        MyClass obj = new MyClass();
-        System.out.println(obj.x);
-    }
-}
