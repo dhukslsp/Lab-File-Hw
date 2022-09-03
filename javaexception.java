f94ea90f0b384da73435eba6eeee84da2180834e
Dhruv SIngh <dhruv1647>
2022-09-03 23:02:42 +0530

    New Commit stateed

--- javaexception.java
+++ javaexception.java
@@ -1,4 +1,22 @@
-public class javaexception{
-	int encap = 12;
-	
+class Employee
+{
+  private int id;
+  
+  public void setId(int newId)
+  {
+    id = newId;
+  }
+  public int getId()
+  {
+    return id;
+  }
 }
+public class javaexception
+  {
+    public static void main (String args[])
+    {
+      Employee emp = new Employee ();
+      emp.setId (100);
+      System.out.println (emp.getId());
+    }
+  }
